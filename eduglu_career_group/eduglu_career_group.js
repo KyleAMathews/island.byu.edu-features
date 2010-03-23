ecaff67cf86e37cf7dd334a953ad0851f9bc793a
Kyle Mathews <mathews.kyle@gmail.com>
2010-03-23 11:48:52 -0600

    Updates to code.

--- eduglu_career_group/eduglu_career_group.js
+++ eduglu_career_group/eduglu_career_group.js
@@ -2,6 +2,7 @@ if (Drupal.jsEnabled) {
   $(document).ready(function() {
     $('.alumni-profile-toggle').click( function() {
       $(this).parent().parent().next().toggle();
+      return false;
     });
   });
 }
